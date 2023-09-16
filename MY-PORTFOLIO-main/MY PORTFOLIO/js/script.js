c1b6a853a73ee53f6f32e321cb5dbf690381bf5b
Johnson Godwin <97678195+kushyray@users.noreply.github.com>
2023-09-16 17:03:10 +0100

    Add files via upload

---
+++ MY-PORTFOLIO-main/MY PORTFOLIO/js/script.js
@@ -0,0 +1,8 @@
+"use strict"
+    // TYPING ANIMATION
+    var typed = new Typed('.typing',{
+        strings:[" ","Web Designer","Web Developer","Front-End Engineer","Programmer","Desktop Publisher"],
+        typeSpeed:100,
+        BackSpeed:20,
+        loop:true
+    });
